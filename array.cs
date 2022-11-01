9fd2330d35e1e3e7cb623a4288cc65d3a9231acc
UzeyirN <7qzjlm4@code.edu.az>
2022-11-01 23:02:52 +0400

    array task

---
+++ array.cs
@@ -0,0 +1,67 @@
+﻿#region arithmeticmean
+
+//int[] arr = { 10, 20, 30, 40 };
+//int sum = 0;
+//int arithmeticmean = 0;
+//for (int i = 0; i < arr.length; i++)
+//{
+//    sum += arr[i];
+//}
+//console.writeline(sum);
+
+//arithmeticmean = sum / arr.length;
+//{
+//    console.writeline(arithmeticmean);
+//}
+#endregion
+
+
+
+#region + and -
+//int[] arr = { 10, 20, -30, 40, -50 };
+//int numP = 0;
+//int numN = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > 0)
+
+//    {
+//        numP++;
+//    }
+
+//    else if (arr[i] < 0)
+//    {
+//        numN++;
+//    }
+//}
+//Console.WriteLine("Number of Positive");
+//Console.WriteLine(numP);
+//Console.WriteLine("Number of Negative");
+//Console.WriteLine(numN);
+
+
+
+
+#endregion
+
+
+#region max num
+
+//int[] arr = { 10, 20, 30, 40, 100 };
+//int maxNum=0;
+//for (int i = 0;i < arr.Length;i++)
+//{
+//    if (maxNum < arr[i])
+//    {
+//        maxNum = arr[i];
+        
+//    }
+    
+
+        
+//}
+//Console.WriteLine("Max num:");
+//Console.WriteLine(maxNum);
+
+#endregion
